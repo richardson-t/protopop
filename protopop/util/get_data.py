bbdf5df3a2a3f41d84423d15cd148f9a0f300cbc
Theodore Richardson <richardson.t@login11.ufhpc>
2026-06-17 00:38:07 -0400

    add wrapper for cluster data + adjust accordingly

--- protopop/util/get_data.py
+++ protopop/util/get_data.py
@@ -3,12 +3,25 @@ from subprocess import run
 from shutil import move, unpack_archive
 
 datapath = path.dirname(__file__)
+record_id = None #change when you make it
 
-def retriever():
-    if path.exists(f'{datapath}/../data'):
-        raise RuntimeError('data files already exist; move or delete them in order to retrieve files from the repository')
+def retrieve_tracks():
+    if path.exists(f'{datapath}/../track_data'):
+        raise RuntimeError('track data files already exist; move or delete them in order to retrieve files from the repository')
 
-    run('uvx zenodo_get {record_id}', shell=True) #put in record id once you make one
-    unpack_archive('data.tar.xz')
-    move('data', f'{datapath}/..')
-    run('rm data.tar.xz', shell=True)
+    fn = 'track_data.tar.xz'
+    
+    run(f'uvx zenodo_get {record_id} -g {fn}', shell=True)
+    unpack_archive(f'{fn}')
+    move('track_data', f'{datapath}/..')
+    run(f'rm {fn}', shell=True)
+
+def retrieve_clusters():
+    if path.exists(f'cluster_data'):
+        raise RuntimeError('cluster data files already exist; move or delete them in order to retrieve files from the repository')
+
+    fn = 'cluster_data.tar.xz'
+    
+    run(f'uvx zenodo_get {record_id} -g {fn}', shell=True)
+    unpack_archive(f'{fn}')
+    run(f'rm {fn}', shell=True)
